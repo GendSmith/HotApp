66dc2afc4f2a9cfac8f6bb21850f0430d2e496da
Gend Smith <1439786496@qq.com>
2018-03-10 14:51:46 +0800

    feedback的js逻辑

--- pages/feedback/feedback.js
+++ pages/feedback/feedback.js
@@ -1,66 +1,36 @@
-// pages/feedback/feedback.js
+var app = getApp();
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
+  data:{
+    content:""
   },
+  onSubmit:function(event){
+    if(!even.detail.value.content){
+      wx.showToast({
+        title: '请填写反馈内容～'
+      });
+      return;
+    }
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    var that = this;
+    this.setData({
+      content: event.detail.value.content
+    });
+    var now = Data.parse(new Data());
+    var key = app.globalData.hotapp.getPrefix('feedback');
+    app.globalData.hotapp.post(key,this.data.content,function(res){
+      if(res.ret == 0) {
+        wx.showToast({
+          title:"提交成功"
+        });
+        wx.redirectTo({
+          url: '../feedback/feedback',
+        });
+      } else{
+        wx.showToast({
+          title: '提交失败',
+        });
+      }
+    });
   }
 })
